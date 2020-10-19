cf120d5c9ab8ae616df4f9f72fbcd07fd9b74c08
Tessa Woodard <tessawoodard@outlook.com>
2020-10-19 10:48:51 -0400

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from './logo.png'
 
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker
+            <img className="logo" src={logo} alt="title" />
         </h2>
     )
 }
